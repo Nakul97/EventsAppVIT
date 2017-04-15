497a2d1e1431582953d3d0d6bbef7a35ce937a1a
Nakul97 <nakul.sakhuja@gmail.com>
2017-04-15 17:31:06 +0530

    Remove Pods

--- Pods/Target Support Files/Pods-EventsAppVIT/Pods-EventsAppVIT-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EventsAppVIT : NSObject
-@end
-@implementation PodsDummy_Pods_EventsAppVIT
-@end
--- Pods/Target Support Files/Pods-EventsAppVIT/Pods-EventsAppVIT-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_EventsAppVITVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_EventsAppVITVersionString[];
-
